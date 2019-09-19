8fec2f115c14cf47b50cca5641ac72b18afa634b
StephenG8403 <kodavoid@gmail.com>
2019-09-19 13:47:52 -0400

    api cleanup

--- main.py
+++ main.py
@@ -3,20 +3,19 @@
 print("Hello! This is the main body of code for the program.")
 import smtplib
 import config
-def send_email(subject, msg):
-        try:
-            server = smtplib.SMTP('smtp.gmail.com', 587)
-            server.ehlo()
-            server.starttls()
-            server.login(config.EMAIL_ADDRESS, config.PASSWORD)
-            message = 'Subject: {}/m/m{}'.format(subject, msg)
-            server.sendmail(config.EMAIL_ADDRESS, config.EMAIL_ADDRESS, msg)
-            server.quit()
-            print("success!")
-        except:
-            print("failure!")
 subject = "Hello!"
 message: str = "Hello, how are you?"
-def send_email(subject, message):
-    pass
-send_email(message, str)
+
+try:
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login(config.EMAIL_ADDRESS, config.PASSWORD)
+    message = 'Subject: {}/m/m{}'.format(subject, msg)
+    server.sendmail(config.EMAIL_ADDRESS, config.EMAIL_ADDRESS, message)
+    server.quit()
+    print("success!")
+except:
+    print("failure!")
+
+
